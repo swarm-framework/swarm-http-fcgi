77fe4e7b9009cd33449875d2152e029721e2fcbe
dami <dami@creabox.net>
2017-05-30 00:42:39 +0200

    Add request decoder  - Version  - Method  - Headers  - URI  - Parameters

--- Sources/swarm/http/fcgi/FastCGIRequestDecoder.cxx
+++ Sources/swarm/http/fcgi/FastCGIRequestDecoder.cxx
@@ -17,13 +17,13 @@
 
 #include "FastCGIRequestDecoder.hxx"
 
-#include <sstream>
+#include <cxxlog/Logger.hxx>
 #include <map>
 #include <memory>
+#include <sstream>
 #include <swarm/http/message/commons/HTTPVersion.hxx>
 #include <swarm/http/message/header/HTTPHeader.hxx>
 #include <swarm/http/message/header/HTTPHeaders.hxx>
-#include <cxxlog/Logger.hxx>
 
 namespace swarm {
     namespace http {
@@ -32,7 +32,7 @@ namespace swarm {
             void FastCGIRequestDecoder::updateBuilder(HTTPRequestBuilder &builder, FCGX_Request &fcgxRequest) {
 
                 static auto logger = LOGGER(FastCGIRequestDecoder);
-                
+
                 // Options
                 std::map<std::string, std::string> options_;
 
@@ -48,7 +48,7 @@ namespace swarm {
 
                     std::stringstream ssKey{};
                     char *c = *envp;
-                    
+
                     // Read key (and detect HTTP header)
                     for (int i = 0; *c != '\0'; ++c, ++i) {
 
@@ -71,48 +71,49 @@ namespace swarm {
                         ssValue << *c;
                     }
 
+                    // logger.log(cxxlog::Level::INFO, [&](std::ostream & os) { os << "arg: " << ssKey.str() << ":" <<
+                    // ssValue.str(); });
+
                     if (header) {
-                        
+
                         // Find header
                         auto header = HTTPHeaders::get(ssKey.str());
-                        
+
                         // Add header to builder
                         builder.addHeaderParam(*header, ssValue.str());
-                        
+
                     } else {
                         // Add to options_
                         options_[ssKey.str()] = ssValue.str();
                     }
-                    
-                    try {
-                        // Read version
-                        builder.version(HTTPVersions::get(options_.at("SERVER_PROTOCOL")));
-                    } catch(...) {
-                        logger.log(cxxlog::Level::SEVERE, "Unable to decode HTTP version");
-                    }
-                    
-                    try {
-                        // Read path
-                        builder.path(options_.at("PATH_INFO"));
-                    } catch(...) {
-                        logger.log(cxxlog::Level::SEVERE, "Unable to get HTTP path");
-                    }
-                    
-                    try {
-                        // Read uri
-                        builder.uri(options_.at("REQUEST_URI"));
-                    } catch(...) {
-                        logger.log(cxxlog::Level::SEVERE, "Unable to get HTTP uri");
-                    }
-                    
-                    try {
-                        // Read method
-                        builder.method(HTTPMethod::get(options_.at("REQUEST_METHOD")));
-                    } catch(...) {
-                        logger.log(cxxlog::Level::SEVERE, "Unable to decode HTTP method");
-                    }
-                    
-                    // FIXME
+                }
+
+                try {
+                    // Read version
+                    builder.version(HTTPVersions::get(options_.at("SERVER_PROTOCOL")));
+                } catch (...) {
+                    logger.log(cxxlog::Level::SEVERE, "Unable to decode HTTP version");
+                }
+
+                try {
+                    // Read path
+                    builder.path(options_.at("PATH_INFO"));
+                } catch (...) {
+                    logger.log(cxxlog::Level::SEVERE, "Unable to get HTTP path");
+                }
+
+                try {
+                    // Read uri
+                    builder.uri(options_.at("REQUEST_URI"));
+                } catch (...) {
+                    logger.log(cxxlog::Level::SEVERE, "Unable to get HTTP uri");
+                }
+
+                try {
+                    // Read method
+                    builder.method(HTTPMethod::get(options_.at("REQUEST_METHOD")));
+                } catch (...) {
+                    logger.log(cxxlog::Level::SEVERE, "Unable to decode HTTP method");
                 }
             }
         }
